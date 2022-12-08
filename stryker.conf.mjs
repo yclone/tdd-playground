484938a797760350cb2f9499f6166069f3662999
yclone <vinicius_marra@hotmail.com>
2022-12-08 16:27:51 -0300

    Add files via upload

---
+++ stryker.conf.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  testRunner: "command",
+  testRunner_comment:
+    "More information about the command plugin can be found here: URL not found",
+  coverageAnalysis: "off",
+};
+export default config;
